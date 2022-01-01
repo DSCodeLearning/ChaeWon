415927903aa99ea4800a1cda9878e572a0495dc4
Chaewon Kim <cchae1138@gmail.com>
2022-01-01 23:18:23 +0900

    first commit

---
+++ ProjectEuler_Problem3.py
@@ -0,0 +1,20 @@
+# 가장 큰 소인수 구하기
+num = int(input("Enter number: "))
+
+list1 = []
+list2 = []
+
+for i in range(2, num):
+    rem = num % i
+    if rem == 0:
+        list1.append(i)
+
+m = 
+for quo in list1:
+    result = list1[m] % quo
+    if result != 0:
+        list2.append(quo)
+        m += -1
+        if m < 0:
+            break
+    print(list2)
