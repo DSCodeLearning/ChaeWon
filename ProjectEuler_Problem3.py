c7ac143d6b52a317c452bc214305a7e9e73bd5af
Chaewon Kim <cchae1138@gmail.com>
2022-02-07 20:23:44 +0900

    [22.02.07] Completed

--- ProjectEuler_Problem3.py
+++ ProjectEuler_Problem3.py
@@ -1,17 +1,5 @@
 # 가장 큰 소인수 구하기
 
-# num = int(input("Enter number: "))
-
-# list = []
-
-# for i in range(1, num+1):
-#     if num % i == 0:
-#         list.append(i)
-#         num /= i
-
-# print(list)
-# print(list[-1])
-
 num = int(input("Enter number: "))
 
 list = []
