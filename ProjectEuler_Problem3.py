2937560481619d385254a76b7e312c3a3ec222fd
Chaewon Kim <cchae1138@gmail.com>
2022-02-07 19:46:32 +0900

    22.02.07 first try

--- ProjectEuler_Problem3.py
+++ ProjectEuler_Problem3.py
@@ -1,23 +1,21 @@
 # 가장 큰 소인수 구하기
-# [220101] try1
 
 num = int(input("Enter number: "))
 
-list1 = []
-list2 = []
+list = []
 
-for i in range(2, num):
-    rem = num % i
-    if rem == 0:
-        list1.append(i)
+for i in range(1, num+1):
+    if num % i == 0:
+        list.append(i)
 
-m =
+m = len(list) // 2
+list = list[:m+1]
 
-for quo in list1:
-    result = list1[m] % quo
-    if result != 0:
-        list2.append(quo)
-        m += -1
-        if m < 0:
-            break
-    print(list2)
+result_list = []
+
+for m in list:
+    for n in range(1, m+1):
+        if m % n == 0:
+            result_list.append(n)         
+            
+print(result_list)
